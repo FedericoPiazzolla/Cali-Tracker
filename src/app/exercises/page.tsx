3ee35c1f96125d16d33a32ea0f528e53ec45e9b9
FedericoPiazzolla <Federicop257@gmail.com>
2026-02-16 19:10:45 +0100

    filer card with 4 buttons on exercises page

--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -1,13 +1,39 @@
+"use client";
 import ExerciseRow from "@/components/ExerciseRow";
 import { exercises } from "@/data/exercise.mock";
+import { useState } from "react";
 
 export default function Exercises() {
+  const [selectedCategory, setSelectedCategory] = useState("all");
+  const categories = ["all", "pull", "push", "core"];
+
+  function handleCategoryChange(category: string) {
+    setSelectedCategory(category);
+  }
+
+  const fileterdExercises =
+    selectedCategory === "all"
+      ? exercises
+      : exercises.filter((e) => e.category === selectedCategory);
+
+  console.log(selectedCategory);
   return (
     <div className="flex flex-col items-center max-w-xl mx-auto px-4">
       <h1>Exercises</h1>
 
+      <div className="w-72 flex flex-row justify-between my-10">
+        {categories.map((category) => (
+          <button
+            key={category}
+            onClick={() => handleCategoryChange(category)}
+            className="bg-white text-black px-4 rounded-2xl cursor-pointer">
+            {category}
+          </button>
+        ))}
+      </div>
+
       <ul className="flex gap-4">
-        {exercises.map((exercise) => (
+        {fileterdExercises.map((exercise) => (
           <ExerciseRow key={exercise.id} exercise={exercise} />
         ))}
       </ul>
