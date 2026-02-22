796b673a864b5f30ceb00eabdb1009e168ec1313
FedericoPiazzolla <Federicop257@gmail.com>
2026-02-22 18:56:49 +0100

    add: useExercise logi to exercise/page.tsx

--- src/app/exercises/page.tsx
+++ src/app/exercises/page.tsx
@@ -1,11 +1,33 @@
 "use client";
 import ExerciseRow from "@/components/ExerciseRow";
-import { exercises } from "@/data/exercise.mock";
+import { Exercise } from "@/types/exercise";
+//import { exercises } from "@/data/exercise.mock";
 import { useState } from "react";
+import useExercises from "@/hooks/useExercises";
 
 export default function Exercises() {
   const [selectedCategory, setSelectedCategory] = useState("all");
   const categories = ["all", "pull", "push", "core"];
+  const { exercises, setExercises } = useExercises();
+  const [name, setName] = useState("");
+  const [formCategory, setFormCategory] =
+    useState<Exercise["category"]>("pull");
+  const [tracking, setTracking] = useState<Exercise["tracking"]>("reps");
+
+  function handleAddExercise() {
+    if (!name.trim()) return;
+
+    const newExercise: Exercise = {
+      id: crypto.randomUUID(),
+      name: name.trim(),
+      category: formCategory,
+      tracking,
+    };
+
+    setExercises([...exercises, newExercise]);
+
+    setName("");
+  }
 
   function handleCategoryChange(category: string) {
     setSelectedCategory(category);
@@ -23,6 +45,34 @@ export default function Exercises() {
   return (
     <div className="flex flex-col items-center max-w-xl mx-auto px-4">
       <h1>Exercises</h1>
+
+      <div className="bg-white text-black">
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <select
+          name="category"
+          id="category"
+          value={formCategory}
+          onChange={(e) =>
+            setFormCategory(e.target.value as Exercise["category"])
+          }>
+          <option value="pull">Pull</option>
+          <option value="push">Push</option>
+          <option value="core">Core</option>
+        </select>
+        <select
+          name="tracking"
+          id="tracking"
+          value={tracking}
+          onChange={(e) => setTracking(e.target.value as Exercise["tracking"])}>
+          <option value="reps">Reps</option>
+          <option value="seconds">Seconds</option>
+        </select>
+        <button onClick={handleAddExercise}>Add</button>
+      </div>
 
       <div className="w-72 flex flex-row justify-between my-10">
         {categories.map((category) => (
